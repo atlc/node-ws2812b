5a9a8c1c525d91d492e742f11cfa605fadd08af1
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-09-24 22:00:58 -0500

    Testing out split channels with alternating light looping test

--- lights.js
+++ lights.js
@@ -5,15 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const rpi_ws281x_native_1 = __importDefault(require("rpi-ws281x-native"));
 const LED_COUNT = 300; // 5 meters at 60 per meter
-const PIN = 6; // GPIO Pin 6 - is pin 31 on board
+const PINS = {
+    strip_one: 5,
+    strip_two: 6 // GPIO Pin 6 - pin 31 on board
+};
 const ORANGE = 0xef6c00;
 const PURPLE = 0x8e24aa;
-const channel = (0, rpi_ws281x_native_1.default)(LED_COUNT, {
-    brightness: 127,
-    gpio: PIN,
-    stripType: rpi_ws281x_native_1.default.stripType.WS2812
+const channels = rpi_ws281x_native_1.default.init({
+    channels: [
+        { count: LED_COUNT, gpio: PINS.strip_one, brightness: 127, stripType: rpi_ws281x_native_1.default.stripType.WS2812 },
+        { count: LED_COUNT, gpio: PINS.strip_two, brightness: 127, stripType: rpi_ws281x_native_1.default.stripType.WS2812 },
+    ]
 });
-for (let i = 0; i < channel.array.length; i++) {
-    channel.array[i] = i % 2 ? ORANGE : PURPLE;
-}
+const [front, rear] = channels;
+front.array.forEach((led, i) => {
+    led = i % 2 ? ORANGE : PURPLE;
+});
 rpi_ws281x_native_1.default.render();
+function loopAlternatingColors(delay) {
+    setTimeout(() => {
+        front.array.forEach((led) => {
+            led = led === PURPLE ? ORANGE : PURPLE;
+        });
+        rpi_ws281x_native_1.default.render();
+    }, delay);
+}
+loopAlternatingColors(4000);
--- lights.ts
+++ lights.ts
@@ -1,19 +1,35 @@
 import ws281x from 'rpi-ws281x-native';
 
 const LED_COUNT = 300; // 5 meters at 60 per meter
-const PIN = 6; // GPIO Pin 6 - is pin 31 on board
+const PINS = {
+    strip_one: 5, // GPIO Pin 5 - is pin 29 on board
+    strip_two: 6 // GPIO Pin 6 - pin 31 on board
+}
 
 const ORANGE = 0xef6c00;
 const PURPLE = 0x8e24aa;
 
-const channel = ws281x(LED_COUNT, {
-    brightness: 127,
-    gpio: PIN,
-    stripType: ws281x.stripType.WS2812
+const channels = ws281x.init({
+    channels: [
+        { count: LED_COUNT, gpio: PINS.strip_one, brightness: 127, stripType: ws281x.stripType.WS2812 },
+        { count: LED_COUNT, gpio: PINS.strip_two, brightness: 127, stripType: ws281x.stripType.WS2812 },
+    ]
+})
+
+const [front, rear] = channels;
+
+front.array.forEach((led, i) => {
+    led = i%2 ? ORANGE : PURPLE
 });
 
-for (let i = 0; i < channel.array.length; i++) {
-    channel.array[i] = i % 2 ? ORANGE : PURPLE;
-}
-
 ws281x.render();
+
+function loopAlternatingColors (delay: number) {
+    setTimeout(() => {
+        front.array.forEach((led) => {
+            led = led === PURPLE ? ORANGE : PURPLE
+        });
+        ws281x.render();
+    }, delay)
+}
+loopAlternatingColors(4000);
